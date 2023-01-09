1c25b4e79a953683d056768b2423ae4679442112
Anshu <imanshu822@gmail.com>
2023-01-09 23:26:30 +0530

    Problem 4 edited

--- DAY-6/P4- 771. Jewels and Stones (LeetCode).cpp
+++ DAY-6/P4- 771. Jewels and Stones (LeetCode).cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int ans = 0;
-        for(int i = 0; i < stones.size(); i++){
+        for(int i = 0; i < jewels.size(); i++){
             ans += count(stones.begin(), stones.end(), jewels[i]);
         }
         return ans;
