fa9b6165077cf2aa632ee653da308e78f1480f37
Anshu <imanshu822@gmail.com>
2023-01-09 23:21:07 +0530

    Probme 4 is solved

--- DAY-6/P4- 771. Jewels and Stones (LeetCode).cpp
+++ DAY-6/P4- 771. Jewels and Stones (LeetCode).cpp
@@ -1,1 +1,14 @@
+// Problem Link: https://leetcode.com/problems/jewels-and-stones/description/
+// Problem Name: 771. Jewels and Stones (LeetCode)
+// Solution: accepted Submission
 
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans = 0;
+        for(int i = 0; i < stones.size(); i++){
+            ans += count(stones.begin(), stones.end(), jewels[i]);
+        }
+        return ans;
+    }
+};
