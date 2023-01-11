a96d1000c3517358088039f5fb030e0bf733d04c
Anshu <93573185+imanshu822@users.noreply.github.com>
2023-01-11 13:20:36 +0530

    some changes made in Problem 4

--- DAY-4/P4- 160. Intersection of Two Linked Lists (LeetCode).cpp
+++ DAY-4/P4- 160. Intersection of Two Linked Lists (LeetCode).cpp
@@ -1,17 +1,31 @@
-// Problem Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+// Problem Link: https://leetcode.com/problems/intersection-of-two-linked-lists/description/
 // Problem Name: 160. Intersection of Two Linked Lists (LeetCode)
 // Solution: accpted submission
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> umap;
-        for(int i = 0; i < nums1.size(); i++){
-            umap[nums[i]]++;
-        }
-        vector<int>v;
-        for(int i = 0; i < nums2.size(); i++){
-            if(umap.find(nums[i]) != nums)
-        }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+            
+        ListNode *h1 = headA;
+        int c1 = 0, c2 = 0;
+       while(h1){
+           ListNode *temp = headB;
+           while(temp){
+               if(h1 == temp){
+                   return h1;
+               }
+                temp = temp->next;
+           }
+           h1 = h1->next;
+       }
+        return h1;
     }
 };
