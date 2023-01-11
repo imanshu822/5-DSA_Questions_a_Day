fea7e5fa141e727d5b3cdbc4e877ed7f1a6df136
Anshu <imanshu822@gmail.com>
2023-01-11 11:28:37 +0530

    Problem 2 solved

--- DAY-7/P2- 371. Sum of Two Integers without using + operator (LeetCode).cpp
+++ DAY-7/P2- 371. Sum of Two Integers without using + operator (LeetCode).cpp
@@ -1,1 +1,19 @@
+// Problem Link: https://leetcode.com/problems/sum-of-two-integers/description/
+// Problem Name: 371. Sum of Two Integers without using + operator (LeetCode)
+// Solution: accepted submission
 
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(a > 0){
+            b++;
+            a--;
+        }
+        while(a < 0){
+            b--;
+            a++;
+        }
+        return b;
+        
+    }
+};
