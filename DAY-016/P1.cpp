f8ee985e617448911a4b7253e4078304439faf9c
Anshu <imanshu822@gmail.com>
2023-01-19 22:12:39 +0530

    Problem 1 solved

---
+++ DAY-016/P1.cpp
@@ -0,0 +1,19 @@
+// Problem Link: https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+// Problem Name: 974. Subarray Sums Divisible by K
+// Solution: accepted Submission
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int count = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            int remainder = (sum % k + k) % k;
+            count += map[remainder];
+            map[remainder]++;
+        }
+    return count;
+    }
+};
