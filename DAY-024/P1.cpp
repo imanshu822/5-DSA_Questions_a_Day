acca2a5a2f14a3b5b8a01540b3fb2c6b0ee5e764
Anshu <imanshu822@gmail.com>
2023-01-28 00:19:25 +0530

    Problem 1 Solved

---
+++ DAY-024/P1.cpp
@@ -0,0 +1,39 @@
+// Problem Link: https://leetcode.com/problems/concatenated-words/description/
+// Problem Name: 472. Concatenated Words (LeetCode)
+// Solution: accepted submission
+
+class Solution {
+public:
+    set<string> dict;
+    bool check(string &word,int c){
+        if(word.size()==0){
+            if(c>=2)
+                return true;
+            return false;
+        }
+        for(int i=1;i<=word.size();i++){
+            string left=word.substr(0,i);
+            string right=word.substr(i);
+            if(dict.find(left)!=dict.end()){
+                ++c;
+                if(check(right,c)){
+                    return true;
+                }
+                --c;
+            }
+        }
+        return false;
+    }
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string> ans;
+        for(auto &word:words){
+            dict.insert(word);
+        }
+        for(auto &word:words){
+            if(check(word,0)){
+                ans.push_back(word);
+            }
+        }
+        return ans;
+    }
+};
