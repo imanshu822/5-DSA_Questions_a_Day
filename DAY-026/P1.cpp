88fabdd09b0d57604abb43647005a3d564b2d7e8
Anshu <imanshu822@gmail.com>
2023-01-30 23:12:16 +0530

    Problem 1 solved

---
+++ DAY-026/P1.cpp
@@ -0,0 +1,16 @@
+// Problem Link: https://leetcode.com/problems/n-th-tribonacci-number/description/
+// Problem Name: 1137. N-th Tribonacci Number (LeetCode)
+// Solution: accepted submission
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int>temp={0,1,1};
+        int var=0;
+        for(int i=3;i<n+1;++i){
+           var=temp[i-1]+temp[i-2]+temp[i-3];
+           temp.push_back(var);
+        }
+        return temp[n];
+    }
+};
