dff5ce13d2fb9b5dc06cfcbf9a3658d812e12e93
Anshu <imanshu822@gmail.com>
2023-08-26 00:17:11 +0530

    Time: 1 ms (91.57%), Space: 43.6 MB (14.9%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        Integer[][][] dp = new Integer[101][101][201];
+        Integer[][] dp = new Integer[101][101];
         return solve(s1, s2, s3, 0, 0, 0, dp) == 1 ? true : false;
     }
-    private int solve(String s1, String s2, String s3, int i, int j, int k , Integer[][][] dp) {
+    private int solve(String s1, String s2, String s3, int i, int j, int k , Integer[][] dp) {
         if(i == s1.length() && j == s2.length() && k == s3.length()) {
             return 1;
         }
         if(k == s3.length()) {
             return 0;
         }
-        if(dp[i][j][k] != null) {
-            return dp[i][j][k];
+        if(dp[i][j] != null) {
+            return dp[i][j];
         }
         int result = 0;
         if(i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
@@ -21,6 +21,6 @@ class Solution {
         if(j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
             result = solve(s1, s2, s3, i, j + 1, k + 1, dp);
         }
-        return dp[i][j][k] = result;
+        return dp[i][j] = result;
     }
 }
