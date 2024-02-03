0eb810222c449e283aa00442a20e94a8d991dfcb
Anshu <imanshu822@gmail.com>
2024-02-04 03:10:50 +0530

    Time: 7 ms (92.29%), Space: 11 MB (15.42%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int dp[501];
-    int solve(int i, vector<int>& arr, int k){
-        if(i >= arr.size())
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        int result = 0;
-        int currMax = -1;
-        for(int j = i; j < arr.size() && (j-i+1) <= k; j++){
-            currMax = max(currMax, arr[j]);
-            
-            result = max(result, (j-i + 1) * currMax + solve(j+1, arr, k));
-            dp[i] = result;
-        }
-        return result;
-    }
-
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, arr, k);
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+        for(int i = 1; i<= n; i++){
+            int currMax = -1;
+            for(int j = 1; j <= k && i-j >= 0; j++){
+                currMax = max(currMax, arr[i-j]);
+                dp[i] = max(dp[i], (currMax * j) + dp[i-j]); 
+            }
+        }
+        return dp[n];
     }
 };
