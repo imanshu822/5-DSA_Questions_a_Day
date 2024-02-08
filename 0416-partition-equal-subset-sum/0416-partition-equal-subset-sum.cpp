410e3de1b634c70a77176afc77ada36345d1b999
Anshu <imanshu822@gmail.com>
2024-02-08 20:09:00 +0530

    Time: 59 ms (93.77%), Space: 20.8 MB (57.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,38 +1,35 @@
 class Solution {
-    bool subsetSum(vector<int> arr, int sum){
-        int n = arr.size();
+public:
+    bool solve(vector<int>& nums, int sum){
+        int n = nums.size();
         int dp[n+1][sum+1];
-        for(int i = 0; i < n+1; i++){
-            for(int j = 0; j < sum+1; j++){
-                if(i == 0)
-                    dp[i][j] = false;
-                else if( j == 0)
-                    dp[i][j] = true;
-            }
+        memset(dp, -1, sizeof(dp));
+        for(int col = 0; col < sum+1; col++){
+            dp[0][col] = false;
         }
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < sum+1; j++){
-                if(arr[i-1] <= j){
-                    dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
-                }
-                else{
+        for(int row = 0; row < n+1; row++){
+            dp[row][0] = true;
+        }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < sum + 1; j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j - nums[i-1]] || dp[i-1][j];
+                }else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
         return dp[n][sum];
     }
-
-public:
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sum+= nums[i];
+        int n = nums.size();
+        int numsSum = 0;
+        for(auto x: nums){
+            numsSum += x;
         }
-        if(sum % 2 != 0)
+        if(numsSum & 1)
             return false;
-        else{
-            return subsetSum(nums, sum/2);
-        }
+        int numsHalfSum = numsSum/2;
+        return solve(nums, numsHalfSum);
     }
 };
