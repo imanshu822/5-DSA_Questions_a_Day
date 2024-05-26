eea6b25898734f3eea48e819eb4f5a0bd4c54cbc
Anshu <imanshu822@gmail.com>
2024-05-26 13:04:55 +0530

    Time: 9 ms (21.61%), Space: 12 MB (48.39%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,16 +3,36 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-        int targetRow = row - 1;
-        for(int i = 0; i < row-1; i++){
-            if(matrix[i][0] <= target && matrix[i+1][0] > target){
-                targetRow = i;
+        int low = 0, high = row - 1;
+        int targetRow = -1;
+
+        // Correct binary search to find the correct row
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target >= matrix[mid][0] && target <= matrix[mid][col - 1]) {
+                targetRow = mid;
                 break;
+            } else if (target < matrix[mid][0]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        for(int i = 0; i < col; i++){
-            if(matrix[targetRow][i] == target)
+
+        // If the target row was not found, return false
+        if (targetRow == -1) return false;
+
+        // Reset boundaries for column search
+        int left = 0, right = col - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (matrix[targetRow][mid] == target) {
                 return true;
+            } else if (matrix[targetRow][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         return false;
     }
