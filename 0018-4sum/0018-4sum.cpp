f3d598536b251e4d3d9f8afb26d5b3f4339a145d
Anshu <imanshu822@gmail.com>
2024-05-28 13:49:46 +0530

    Time: 25 ms (67.83%), Space: 15.9 MB (50.32%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> st;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
         for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i+1; j < n; j++){
-                set<long long> hashSet;
-                for(int k = j+1; k < n; k++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while(k < l){
                     long long sum = nums[i] + nums[j];
                     sum += nums[k];
-                    long long fourth = target - (sum);
-                    
-                    if(hashSet.find(fourth) != hashSet.end()){
-                        int x = int (fourth);
-                        vector<int> temp = {nums[i], nums[j], nums[k], x};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+                    sum += nums[l];
+                    if( sum == target){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        result.push_back(temp);
+                        k++; l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    }else if(sum < target){
+                        k++;
+                    }else {
+                        l--;
                     }
-                    hashSet.insert(nums[k]);
                 }
             }
         }
-        vector<vector<int>> result(st.begin(), st.end());
         return result;
     }
 };
