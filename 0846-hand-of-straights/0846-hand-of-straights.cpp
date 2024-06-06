e697c075cd39eb1b998250b8853335436ed0af1b
Anshu <imanshu822@gmail.com>
2024-06-06 20:52:06 +0530

    Time: 72 ms (23.99%), Space: 30.7 MB (54.6%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
-        int next = hand[i] + 1;
-        hand[i] = -1;  // Mark as used
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == next) {
-                next = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        return count == groupSize;
-    }
+    bool isNStraightHand(vector<int>& nums, int k) 
+    {
+        if(nums.size() % k) return false;
 
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize != 0) return false;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findSuccessors(hand, groupSize, i, n)) return false;
+        map<int,int>mp;
+        for(auto val:nums) mp[val]++;
+
+        int n = nums.size();
+        while(n--)
+        {
+            for(auto [a, b]:mp)
+            {
+                for(int i=a;i<a+k;i++)
+                {
+                    if(mp[i] == 0) return false;
+                    mp[i]--;
+                    if(mp[i]==0) mp.erase(i);
+                }
+                break;
             }
         }
         return true;
