46edd37cf4dda039b14fd9f52197050c37b1c3d9
Anshu <imanshu822@gmail.com>
2024-06-24 08:47:03 +0530

    Time: 54 ms (95.83%), Space: 54.6 MB (69.83%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int n = nums.size();
-        int res = 0;
-        
-        for (int i = 0; i < n; i++) {
-            priority_queue<int> max_heap;
-            priority_queue<int, vector<int>, greater<int>> min_heap;
-            
-            for (int j = i; j < n; j++) {
-                max_heap.push(nums[j]);
-                min_heap.push(nums[j]);
-                
-                int max_val = max_heap.top();
-                int min_val = min_heap.top();
-                
-                if (max_val - min_val <= limit) {
-                    res = max(res, j - i + 1);
-                } else {
-                    break; 
-                }
+        deque<int> increase;
+        deque<int> decrease;
+        int max_len = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            while (!increase.empty() && nums[right] < increase.back()) {
+                increase.pop_back();
             }
+            increase.push_back(nums[right]);
+
+            while (!decrease.empty() && nums[right] > decrease.back()) {
+                decrease.pop_back();
+            }
+            decrease.push_back(nums[right]);
+
+            while (decrease.front() - increase.front() > limit) {
+                if (nums[left] == decrease.front()) {
+                    decrease.pop_front();
+                }
+                if (nums[left] == increase.front()) {
+                    increase.pop_front();
+                }
+                ++left;
+            }
+
+            max_len = std::max(max_len, right - left + 1);
         }
-        
-        return res;
+
+        return max_len;
     }
 };
