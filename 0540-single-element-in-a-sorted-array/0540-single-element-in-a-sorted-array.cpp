942bbee393cf881492aee63f611ccb28f7c84042
Anshu <imanshu822@gmail.com>
2024-06-30 04:43:22 +0530

    Time: 66 ms (6.29%), Space: 22.4 MB (100%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int, int> umap;
-        for(int it: nums){
-            umap[it]++;
+        int n = nums.size();
+        int curr_xor=0; //variable will store XOR
+        for(int i =0;i<n;i++){
+            curr_xor ^= nums[i]; // XOR
         }
-        for(auto& it: umap){
-            if(it.second == 1)
-                return it.first;
-        }
-        return -1;
+        return curr_xor;
     }
 };
