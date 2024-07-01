7c6cf1abca4cfb4595e48857a37e56e586f41610
Anshu <imanshu822@gmail.com>
2024-07-01 14:57:20 +0530

    Time: 0 ms (100%), Space: 13.6 MB (37.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,10 +1,42 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target)
-                return i;
+        int n = nums.size();
+        if (n == 0) return -1;
+
+        // Find the pivot
+        int low = 0, high = n - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
+
+        int pivot = low;
+        low = 0;
+        high = n - 1;
+
+        // Determine which part of the array to search
+        if (target >= nums[pivot] && target <= nums[high]) {
+            low = pivot;
+        } else {
+            high = pivot;
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+ 
         return -1;
     }
 };
