506d066797abbd9816b768e6dc2c4e3c31efb9c5
Anshu <imanshu822@gmail.com>
2024-07-18 14:13:16 +0530

    Time: 3 ms (82.24%), Space: 12.2 MB (31.3%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> nextGreaterMap;
-        stack<int> st;
-
-        for(int num : nums2){
-            while(!st.empty() && st.top() < num){
-                nextGreaterMap[st.top()] = num;
-                st.pop();
-            }
-            st.push(num);
+        unordered_map<int, int> umap;
+        for(int i = 0; i < nums2.size(); i++){
+            umap[nums2[i]] = i;
         }
 
-        while(!st.empty()){
-            nextGreaterMap[st.top()] = -1;
-            st.pop();
-        }
-
-        vector<int> result;
         for(int i = 0; i < nums1.size(); i++){
-            result.push_back(nextGreaterMap[nums1[i]]);
-        }
-        return result;
+            if(umap.find(nums1[i]) != umap.end()){
+                for(int j = umap[nums1[i]]; j < nums2.size(); j++){
+                    if(nums2[j] > nums1[i]){
+                        nums1[i] = nums2[j];
+                        break;
+                    }else if(j == nums2.size()-1){
+                        nums1[i] = -1;
+                    }
+                }
+            }else{
+                nums1[i] = -1;
+            }
+        } 
+        return nums1;  
     }
 };
