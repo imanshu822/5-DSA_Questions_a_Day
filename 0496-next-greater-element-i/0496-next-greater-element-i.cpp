7a2efb2459a1aed4d0be0211364bfc7dbc7dccf5
Anshu <imanshu822@gmail.com>
2024-07-18 16:52:19 +0530

    Time: 4 ms (70.07%), Space: 12.5 MB (6.77%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int l1 = nums1.size(), l2 = nums2.size();
+        stack<int> st;
+        vector<int> store(l2, -1);
+
         unordered_map<int, int> umap;
-        for(int i = 0; i < nums2.size(); i++){
+        for (int i = 0; i < nums2.size(); i++) {
             umap[nums2[i]] = i;
         }
 
-        for(int i = 0; i < nums1.size(); i++){
-            if(umap.find(nums1[i]) != umap.end()){
-                for(int j = umap[nums1[i]]; j < nums2.size(); j++){
-                    if(nums2[j] > nums1[i]){
-                        nums1[i] = nums2[j];
-                        break;
-                    }else if(j == nums2.size()-1){
-                        nums1[i] = -1;
-                    }
-                }
+        for (int i = 0; i < l2; i++) {
+            while (!st.empty() && nums2[st.top()] < nums2[i]) {
+                store[st.top()] = nums2[i];
+                st.pop();
             }
-        } 
-        return nums1;  
+            st.push(i);
+        }
+
+        for (int i = 0; i < l1; i++) {
+            nums1[i] = store[umap[nums1[i]]];
+        }
+
+        return nums1;
     }
 };
