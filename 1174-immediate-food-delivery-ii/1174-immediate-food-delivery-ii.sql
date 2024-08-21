e6ba2f5bb9ee047afb68f7fdc17af7426dfad1e2
Anshu <imanshu822@gmail.com>
2024-08-21 17:10:43 +0530

    Time: 639 ms (37.26%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,21 +1,18 @@
-SELECT 
-    ROUND(
-        SUM(
-            CASE
-                WHEN order_date = customer_pref_delivery_date THEN 1
-                ELSE 0
-            END
-        ) * 100 / COUNT(*), 2
-    ) AS immediate_percentage
-FROM
-    Delivery
+-- SELECT ROUND(SUM(if(order_date = customer_pref_delivery_date, 1, 0)) * 100 / count(customer_id), 2) as 
+-- immediate_percentage
+-- FROM Delivery 
+-- WHERE
+-- (customer_id, order_date) IN
+--     (SELECT customer_id, MIN(order_date) AS first_order_date
+--     FROM Delivery
+--     GROUP BY customer_id);
+
+
+SELECT ROUND(avg(order_date = customer_pref_delivery_date) * 100 , 2) as 
+immediate_percentage
+FROM Delivery 
 WHERE
-    (customer_id, order_date) IN(
-        SELECT 
-            customer_id, min(order_date)
-        FROM 
-            Delivery
-        GROUP BY 
-            customer_id
-    );
-    
+(customer_id, order_date) IN
+    (SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id);
