f4ddc531c1e4fa08fd12b8cf5fe7418ed4c56b9e
Anshu <imanshu822@gmail.com>
2024-08-29 01:39:25 +0530

    Time: 845 ms (14.88%), Space: 8.3 MB (93.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
+
+    bool solve(string& s, int i, int j){
+        if(i >= j){
+            return true;
+        }
+        if(s[i] == s[j]){
+            return solve(s, i + 1, j - 1);
+        }else{
+            return false;
+        }
+    }
+
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n == 0) return "";
-        
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int maxLength = 1; // Every single character is a palindrome
-        int start = 0;
-        
-        // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = true;
-        }
-        
-        // Check for substring of length 2
-        for (int i = 0; i < n - 1; ++i) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLength = 2;
-            }
-        }
-        
-        // Check for lengths greater than 2
-        for (int len = 3; len <= n; ++len) {
-            for (int i = 0; i < n - len + 1; ++i) {
-                int j = i + len - 1;
-                
-                // Check if the current substring is a palindrome
-                if (dp[i + 1][j - 1] && s[i] == s[j]) {
-                    dp[i][j] = true;
-                    
-                    if (len > maxLength) {
-                        start = i;
-                        maxLength = len;
+        int n = s.length();
+
+        int maxLen = INT_MIN;
+        int sp = 0;
+        for(int i = 0;i < n; i++){
+            for(int j = i; j < n; j++){
+                if(solve(s, i, j) == true){
+                    if(j - i + 1 > maxLen){
+                        maxLen = j - i + 1;
+                        sp = i;
                     }
+                   
                 }
+
             }
         }
-        
-        return s.substr(start, maxLength);
+        return s.substr(sp, maxLen);
     }
 };
