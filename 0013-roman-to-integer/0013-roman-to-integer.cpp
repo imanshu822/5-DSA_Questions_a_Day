1e579e3fbb5a4484054e0f9453705a489dc5bdc1
Anshu <imanshu822@gmail.com>
2024-08-29 12:59:02 +0530

    Time: 24 ms (6.77%), Space: 12.9 MB (55.55%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,49 +2,23 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n = s.size();
+        unordered_map<char, int> umap{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
         int num = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (i + 1 < n) {
-                if (s[i] == 'I' && s[i + 1] == 'V') {
-                    num += 4;
-                    ++i; 
-                } else if (s[i] == 'I' && s[i + 1] == 'X') {
-                    num += 9;
-                    ++i; 
-                } else if (s[i] == 'X' && s[i + 1] == 'L') {
-                    num += 40;
-                    ++i; 
-                } else if (s[i] == 'X' && s[i + 1] == 'C') {
-                    num += 90;
-                    ++i; 
-                } else if (s[i] == 'C' && s[i + 1] == 'D') {
-                    num += 400;
-                    ++i; 
-                } else if (s[i] == 'C' && s[i + 1] == 'M') {
-                    num += 900;
-                    ++i; 
-                } else {
-                    num += getValue(s[i]);
-                }
-            } else {
-                num += getValue(s[i]);
-            }
+        for(int i = 0; i < n; i++){
+            int curr = umap[s[i]];
+            if(i + 1 < n && umap[s[i]] < umap[s[i+1]]){
+                num -= curr;
+            }else
+                num += umap[s[i]];
         }
         return num;
-    }
-
-private:
-    int getValue(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
     }
 };
