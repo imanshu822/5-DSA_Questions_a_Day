db7481fb0e9faa3360d0f53bb37214c6dcb085e1
Anshu <imanshu822@gmail.com>
2024-09-05 18:40:16 +0530

    Time: 0 ms (100%), Space: 14.3 MB (77.03%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,36 +12,38 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        // create a map of {horizontal-distance, {level, {value}};
         map<int, map<int, multiset<int>>> node;
-        
+        // create a queue which sorte pair {TreeNode, pair{horizontal-dist, level}}
         queue<pair<TreeNode*, pair<int, int>>> q;
-        q.push({root, {0, 0}});  
+        q.push({root, {0, 0}});
 
-        while(!q.empty()) {
+        while(!q.empty()){
             auto temp = q.front();
             TreeNode* n = temp.first;
-            int hd = temp.second.first;  
+            int hd = temp.second.first;
             int level = temp.second.second;
             q.pop();
 
             node[hd][level].insert(n->val);
 
-            if(n->left != NULL) {
-                q.push({n->left, {hd - 1, level + 1}});
+            if(n->left != NULL){
+                q.push({n->left,{hd - 1, level + 1}});
             }
-            if(n->right != NULL) {
-                q.push({n->right, {hd + 1, level + 1}});
+            if(n->right != NULL){
+                q.push({n->right, {hd + 1, level}});
             }
         }
 
         vector<vector<int>> res;
-        for(auto it = node.begin(); it != node.end(); it++) {
-            res.push_back({});
-            for(auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
-                res.back().insert(res.back().end(), it2->second.begin(), it2->second.end());
-            }
-        }
 
+        for(auto it = node.begin(); it != node.end(); it++){
+            res.push_back({});
+            for(auto it2 = it->second.begin(); it2 != it->second.end(); it2++){
+                res.back().insert(res.back().end(), it2->second.begin(), it2->second.end());
+            }   
+        }
         return res;
+
     }
 };
