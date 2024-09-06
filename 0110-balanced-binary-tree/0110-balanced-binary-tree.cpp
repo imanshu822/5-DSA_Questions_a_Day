09360db2b339c3331dc9d1a9e78dd69475fad515
Anshu <imanshu822@gmail.com>
2024-09-06 20:37:38 +0530

    Time: 5 ms (84.82%), Space: 21.9 MB (67.09%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int heightOfTree(TreeNode* root){
+    int height(TreeNode* root){
         if(root == nullptr){
             return 0;
         }
-        int left = heightOfTree(root->left);
-        int right = heightOfTree(root->right);
-        return (max(left, right) + 1);
+        int left = height(root->left);
+        int right = height(root->right);
+        return max(left, right) + 1;
     }
+
     bool isBalanced(TreeNode* root) {
         if(root == nullptr){
             return true;
         }
 
-        // Check if the left and right subtrees are balanced
-        if (!isBalanced(root->left) || !isBalanced(root->right)) {
+        // Check if left and right subtrees are balanced
+        if(!isBalanced(root->left) || !isBalanced(root->right)){
             return false;
         }
 
-        // Calculate the height of the left and right subtrees
-        int leftHeight = heightOfTree(root->left);
-        int rightHeight = heightOfTree(root->right);
+        // Check if current node is balanced by comparing heights
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
 
-        // Check the height difference
-        if(abs(leftHeight - rightHeight) > 1) {
+        if(abs(leftHeight - rightHeight) > 1){
             return false;
+        } else {
+            return true;
         }
-        
-        return true;
     }
 };
