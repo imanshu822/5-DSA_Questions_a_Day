aa6f8801c306dab3733c5114955b3ea1cb064a38
Anshu <imanshu822@gmail.com>
2024-10-19 00:54:46 +0530

    Time: 71 ms (28.34%), Space: 10.1 MB (48.34%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,15 +1,21 @@
-#pragma GCC target("popcnt")
 class Solution {
 public:
-    int countMaxOrSubsets(vector<int>& nums) { //O(n*maxValue) time, O(1) storage
-        int maxOr=0; 
-        for(int n:nums) maxOr|=n;
-        int numberOfBadSubsets = 0; //bad subset means subset whose OR is less than maxOR
-        for(int mask=maxOr;mask; mask=(mask-1)&maxOr){ //iterates over subsets of the set bits of the maxOr
-            int cnt = 0; 
-            for(int n:nums) if(!(n&mask)) cnt++; //counts elements of nums that have all 0s in the positions that are 1s in the current mask
-            numberOfBadSubsets += (__builtin_popcount(mask)%2 ? 1 : -1)*((1<<cnt)-1); //inclusion exclusion
+    int countMaxOrSubsets(vector<int>& nums) {
+        int orr = accumulate(begin(nums), end(nums), 0, [&](int a, int b) {
+            return a | b;
+        });
+
+        int n = nums.size(), ans = 0;
+        for (int i = 0; i < (1 << n); i++) {
+            int cur = 0;
+            for (int index = 0; index < n; index++) {
+                if ((i >> index) & 1) {
+                    cur |= nums[index];
+                }
+            }
+            ans += (cur == orr);
         }
-        return (1<<nums.size())-1- numberOfBadSubsets;//total number of subsets minus number of bad subsets
+
+        return ans;
     }
 };
