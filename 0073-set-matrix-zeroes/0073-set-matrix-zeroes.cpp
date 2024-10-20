a44bdbd2dce96d73f111414c15ccb39cbcf08e6a
Anshu <imanshu822@gmail.com>
2024-10-20 23:33:54 +0530

    Time: 0 ms (100%), Space: 17 MB (55.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,23 +4,49 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        unordered_map<int, int> row;
-        unordered_map<int, int> col;
+        bool firstRowZero = false;
+        bool fristColZero = false;
 
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+            if(matrix[i][0] == 0){
+                fristColZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] == 0){
-                    col[j]++;
-                    row[i]++;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }   
+            }
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < n; i++){
+        if(firstRowZero){
             for(int j = 0; j < m; j++){
-                if(row.find(i) != row.end() || col.find(j) != col.end()){
-                    matrix[i][j] = 0;
-                }
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(fristColZero){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
             }
         }
     }
