43d94d504fa8363f1d0df2c18925a3e007437b9d
Anshu <imanshu822@gmail.com>
2024-10-25 22:44:58 +0530

    Time: 66 ms (92.03%), Space: 47.8 MB (75.82%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    unordered_set<string>xx;
+    bool prefixFnd(string&a){
+        if(!xx.size())return true;
+        string x="";
+        int i=1;
+        while(i<a.size()){
+            x+='/';
+           while(i<a.size()&&a[i]!='/'){
+            x+=a[i];
+           i++;
+           }
+           if(xx.count(x))return false;// if there is a path with that prefix return false 
+           i++;
+        }
+        return true; // if there is no path fnd with any of prefixes return true
+    }
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        vector<string>res;
+        sort(folder.begin(),folder.end());
+        for(auto&a:folder){
+            if(prefixFnd(a)){res.push_back(a);
+            xx.insert(a);
+            }
+        }
+        return res;
+    }
+};
