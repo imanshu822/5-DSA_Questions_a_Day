2222f2bb32e9626ffb0e6cf25b5fc1489663a3df
Anshu <imanshu822@gmail.com>
2024-12-19 22:44:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -2,9 +2,21 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
 
+        // for(int i = 0; i < k; i++){
+        //     int min_element_index = min_element(nums.begin(), nums.end()) - nums.begin();
+        //     nums[min_element_index] *= multiplier;
+        // }
+        // return nums;
+
+        priority_queue <int, vector<int>, greater<int> > pq; 
+        for(int i= 0; i < nums.size(); i++){
+            pq.push(nums[i]);
+        }
+
         for(int i = 0; i < k; i++){
-            int min_element_index = min_element(nums.begin(), nums.end()) - nums.begin();
-            nums[min_element_index] *= multiplier;
+            int x = pq.top() * multiplier;
+            pq.pop();
+            pq.push(x);
         }
         return nums;
     }
